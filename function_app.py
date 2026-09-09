07456e0915c0ade250ef9902c4f1f86e3a334326
MATHEUS PESCH ZENERE <matheus.zenere@univille.br>
2026-09-09 20:05:33 -0300

    Trigger HTTP pronto

--- function_app.py
+++ function_app.py
@@ -10,3 +10,25 @@ def timer_trigger_tapra1(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
+
+@app.route(route="http_trigger_tapra2", auth_level=func.AuthLevel.ANONYMOUS)
+def http_trigger_tapra2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        print(name)
+        return func.HttpResponse(f"OLAAAAA, {name}. BOM DIAAAAA!!!!!.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
